3286f6e5645257c64a4b3162e2b90b83c2c59b3f
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 19:59:19 -0300

    ShopUpdate

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,2 +1,10 @@
 import { Link } from "react-router-dom";
 
+const Navbar = ({cartCount})=> {
+    return(
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+        </nav>
+    )
+}
