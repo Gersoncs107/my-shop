32fd443f14663f81b04d3ffadbf1f4bffbdd18d8
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 19:55:31 -0300

    ShopUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+
+function App() {
+  return(
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+      </Routes>
+    </Router>
+  )
+}
