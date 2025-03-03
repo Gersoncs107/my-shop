5d6dfbb883142e9b9079ec5da2a14d038b245427
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 21:50:15 -0300

    ShopUpdate

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,3 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../services/api";
 import ProductCard from "../components/ProductCard";
+
+const Shop = ()=> {
+    const [products, setProducts] = useState([])
+}
