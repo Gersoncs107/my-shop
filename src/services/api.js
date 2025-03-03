ff946a6dae3abd46919bcae1ea76b1cabfed7864
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 21:46:58 -0300

    ShopUpdate

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,14 @@
 import axios from "axios";
 
 const API_URL = "https://fakestoreapi.com/products"
+
+export const fetchProducts = async()=> {
+    try{
+        const response = await axios.get(API_URL)
+        return response.data
+    }
+    catch(error){
+        console.error("Erro ao buscar produtos:", error)
+        return []
+    }
+}
