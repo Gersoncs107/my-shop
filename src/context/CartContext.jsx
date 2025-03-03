d30be08c8efb53f437ccf4cc8328b04b0178cd19
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 22:20:15 -0300

    ShopUpdate

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,28 @@
 import { createContext, useContext, useState } from "react";
 
-const CartContext= createContext()
+const CartContext = createContext();
 
-export const CartProvider = ({children})=>{
-    const [cart,setCart] = useState([])
-}
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + quantity } : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity }];
+      }
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
