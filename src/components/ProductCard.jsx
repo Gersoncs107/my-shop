08ad6414037dce88288e6f56493b165bfbbcf38b
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 22:02:25 -0300

    ShopUpdate

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
-const ProductCard = ()=> {
+const ProductCard = ({product, addToCart})=> {
     const [quantity, setQuantity] = useState(1)
     
     return(
-        
+        <div>
+            <h3>{product.tittle}</h3>
+            <img src={product.image} alt={product.title} width="100" />
+        </div>
     )
 }
