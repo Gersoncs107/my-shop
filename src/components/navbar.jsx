e6ee6ffb3ca3a13adabc97d6aedb4decf6b8e767
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-02 22:25:45 -0300

    ShopUpdate

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 const Navbar = ({cartCount})=> { //A prop cartCount exibirá a quantidade de itens no carrinho.
+    const { cart } = useCart();
+    const cartCount = cart.reduce((acc, item) => acc + item.quantity, 0);
     return(
         <nav>
             <Link to="/">Home</Link>
