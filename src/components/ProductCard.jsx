c22e4643f6d563b4b74f826585d0d74f278f2cae
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-31 23:51:24 -0300

    react

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 
 const ProductCard = ({product, addToCart})=> {
-    const [quantity, setQuantity] = useState(1)
+    const [quantity, setQuantity] = useState(1);
     
     return(
         <div>
-            <h3>{product.tittle}</h3>
+            <h3>{product.title}</h3> {/* Fixed typo: changed 'tittle' to 'title' */}
             <img src={product.image} alt={product.title} width="100" />
             <p>${product.price}</p>
             <div>
-                <button onClick={()=> setQuantity(quantity -1)} disabled= {quantity <= 1}>-</button>
+                <button onClick={()=> setQuantity(quantity - 1)} disabled={quantity <= 1}>-</button>
                 <input 
-                type="number"
-                value={quantity}
-                onChange={(e)=>setQuantity(Number(e.target.value))} />
+                    type="number"
+                    value={quantity}
+                    onChange={(e)=>setQuantity(Number(e.target.value))} 
+                />
                 <button onClick={()=> setQuantity(quantity + 1)}>+</button>
             </div>
-            <button onClick={()=> addToCart(product,quantity)}>Add to Cart</button>
+            <button onClick={()=> addToCart(product, quantity)}>Add to Cart</button>
         </div>
-    )
+    );
 }
 
-export default ProductCard
+export default ProductCard;
